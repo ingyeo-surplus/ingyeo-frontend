564788949cf2438b593edb9df43d738854856e43
veldic <rsual@snu.ac.kr>
2022-02-07 20:20:21 +0900

    Add GridContainer for auto layout

---
+++ src/components/containers/GridContainer.vue
@@ -0,0 +1,26 @@
+<template>
+<div class="grid-container">
+  <div></div>
+  <slot></slot>
+  <div></div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'GridContainer',
+}
+</script>
+
+<style scoped>
+.grid-container {
+  display: grid;
+  grid-template-columns: 1fr 1080px 1fr;
+}
+@media (max-width: 1080px) {
+  .grid-container {
+    display: grid;
+    grid-template-columns: 0px 1fr 0px;
+  }
+}
+</style>
